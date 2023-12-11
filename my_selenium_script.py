7f4d24bd1b68f2e0c3f878bf1b7e1cc1220fd93a
gmuleva <gouravmuleva635@gmail.com>
2023-12-11 12:42:46 +0530

    xyz

--- my_selenium_script.py
+++ my_selenium_script.py
@@ -1,12 +1,19 @@
+#test case-
+# 1-open chrome
+# 2-open url(https://the-internet.herokuapp.com/)
+# 3-checkboxes
+
+import requests
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
 driver_path = 'D:\\automation\\chromedriver.exe'
 service = Service(executable_path=driver_path)
 driver = webdriver.Chrome(service=service)
+driver.get("https://the-internet.herokuapp.com/")
 
-url = 'https://www.google.com'
-driver.get(url)
 
-driver.implicitly_wait(5)
 
 driver.quit()
